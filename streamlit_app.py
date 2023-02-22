d9963c1f4f35387d604f42b8d0893628f9de92e3
jalbertobparra <125593555+jalbertobparra@users.noreply.github.com>
2023-02-22 17:34:16 +0100

    añado cosillas
    añado cosillas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 
 import streamlit
 
-streamlit.title('My parents New')
+streamlit.title('My parents New Healthy Diner')
 
-streamlit.header('Hola')
-streamlit.text('Rolex y Twingo')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
