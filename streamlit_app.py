cf8cdc00499bcdcf17b1671c988ae502b9360b4f
jalbertobparra <125593555+jalbertobparra@users.noreply.github.com>
2023-02-28 19:04:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(my_fruit_list)
 
     
 def get_frutyvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "this_fruit_choice")
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
   
